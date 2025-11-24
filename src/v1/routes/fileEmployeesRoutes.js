8554378e62008a5f13524c0c1c24697106b9abb3
felipe <felipeord9@gmail.com>
2025-11-24 07:22:06 -0500

    cambio de nombre de llegada para docExamenIngreso

--- src/v1/routes/fileEmployeesRoutes.js
+++ src/v1/routes/fileEmployeesRoutes.js
@@ -24,7 +24,7 @@ router.post('/', upload.fields([
     { name: 'Eps' },
     { name: 'CajaCompensacion' },
     { name: 'OtroSi' },
-    { name: 'Ingreso' },
+    { name: 'ExamenIngreso' },
     { name: 'Escolaridad' },
     { name: 'Arl' },
     { name: 'Otros' },
